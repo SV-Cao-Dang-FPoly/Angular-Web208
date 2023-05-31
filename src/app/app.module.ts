45fbb0cb08ad24cdcb48ef60bebfb84bb3a2d32d
Thanh03Dev <trongthanh2003.tn@gmail.com>
2023-05-31 16:00:38 +0700

    feat: update project base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminProductComponent } from './pages/admin/admin-product/admin-product.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductUpdateComponent } from './pages/product-update/product-update.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { BaseLayoutComponent } from './layouts/base-layout/base-layout.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminProductComponent,
+    DashboardComponent,
+    AboutPageComponent,
+    HomePageComponent,
+    PageNotFoundComponent,
+    ProductAddComponent,
+    ProductUpdateComponent,
+    ProductDetailComponent,
+    AdminLayoutComponent,
+    BaseLayoutComponent,
+    NavComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/product-update/product-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.scss']
+})
+export class ProductUpdateComponent {
+
+}
